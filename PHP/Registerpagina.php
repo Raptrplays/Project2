0ac0da80b87450a2218b17ad74c9c1450eba5877
Sing <singfong91@gmail.com>
2023-06-04 19:39:02 +0200

    Php toegevoegd, Giulia pull het.

---
+++ PHP/Registerpagina.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Home - PVV</title>
+</head>
+<body>
+    <header>
+        <h1>Home</h1>
+        <img src="images/1280px-PVV_logo_(2006–present).svg.png" alt="pvvlogo">
+        <nav>
+            <ul class="nav">
+                <li><a href="">Nieuwsberichten</a></li>
+                <li><a href="">Leden</a></li>
+                <li><a href="">Standpunten</a></li>
+                <li><a href="">Lid worden</a></li>
+            </ul>
+        </nav>
+
+        Maak een nieuw account aan:
+        <br>
+        <div class="container">
+            <form action="form">
+                <input type="text" name="naam" placeholder="Uw Naam">
+            </form>
+        </div>
+
+
+
+        </form>
+    </header>
+</body>
+</html>
