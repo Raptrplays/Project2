d0f9a662d629f80598b959f049e5fe2026bee88d
Sing <singfong91@gmail.com>
2023-06-09 09:27:32 +0200

    Folder toch verwijdert

--- PHP/Main.php
+++ PHP/Main.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Public/nieuwsberichten.css">
-    <link rel="stylesheet" href="../Public/Main.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;800&display=swap"  rel="stylesheet">
@@ -19,8 +18,8 @@
             <a href="#"><img class="navimg" src="images/1280px-PVV_logo_(2006–present).svg.png" alt="Logo"></a>
         </div>
         <ul class="navbar-links">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="nieuwsberichten.html">Nieuwsberichten</a></li>
+            <li><a href="../Html/index.html">Home</a></li>
+            <li><a href="../Html/nieuwsberichten.html">Nieuwsberichten</a></li>
             <li><a href="#">Leden</a></li>
             <li><a href="#">Standpunten</a></li>
             <li><a href="#">Doneren</a></li>
