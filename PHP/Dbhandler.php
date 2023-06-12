b51bf6223c8fbbbe865adcab4799653dc942798a
Sing <singfong91@gmail.com>
2023-06-12 09:39:13 +0200

    git commit 12.06

--- PHP/Dbhandler.php
+++ PHP/Dbhandler.php
@@ -1,18 +1,32 @@
 <?php
-
 class dbHandler
 {
-    private $dataSource = "mysql:dbname=project2pvv;host=localhost;";
+    private $dataSource = "mysql:dbname=eindproject;host=localhost;";
     private $userName = "root";
     private $password = "";
 
+   
 
-    public function GetData()
+
+    public function getUserByEmail($email)
     {
         $pdo = new PDO($this->dataSource, $this->userName, $this->password);
-        $statement = $pdo->prepare("SELECT * FROM accounts");
+        $statement = $pdo->prepare("SELECT * FROM accounts WHERE email = :email");
+        $statement->bindParam(":email", $email, PDO::PARAM_STR);
         $statement->execute();
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function createUser($naam, $email, $password)
+    {
+        $pdo = new PDO($this->dataSource, $this->userName, $this->password);
+        $statement = $pdo->prepare("INSERT INTO accounts (naam, email, password) VALUES (:naam, :email, :password)");
+        $statement->bindParam(":naam", $naam, PDO::PARAM_STR);
+        $statement->bindParam(":email", $email, PDO::PARAM_STR);
+        $statement->bindParam(":password", $password, PDO::PARAM_STR);
+        $statement->execute();
     }
 }
+
+
 ?>
--- PHP/Main.php
+++ PHP/Main.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../Public/nieuwsberichten.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;800&display=swap"  rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;800&display=swap" rel="stylesheet">
     <title>Nieuwsberichten</title>
 </head>
 
@@ -24,12 +24,69 @@
             <li><a href="#">Standpunten</a></li>
             <li><a href="#">Doneren</a></li>
             <li><a href="#">Contact</a></li>
-            <li><a href="PHP/Registerpagina.php" class="button">Lid worden</a></li>
+            <li><a href="Main.php" class="button">Lid worden</a></li>
         </ul>
     </nav>
+    <?php
 
+include "dbHandler.php";
+$db = new dbHandler();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $naam = $_POST['naam'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $repeat_password = $_POST['repeat_password'];
 
+    if ($password !== $repeat_password) {
+        echo "Error: Password zijn niet gelijk";
+        exit;
+    }
 
+    // Example: Check if the user with the same email already exists
+    $existingUser = $db->getUserByEmail($email);
+    if ($existingUser) {
+        // Handle the error, display a message, or redirect back to the registration form
+        echo "Error: User with the same email already exists.";
+        exit;
+    }
+
+    // If all checks pass, create the user
+    $db->createUser($naam, $email, $password);
+
+    // Redirect the user to a success page or login page
+    header("Location: account.html");
+    exit;
+}
+
+?>
+
+    <div class="container">
+        <header>Registreer hier!</header>
+        <form action="account.html" method="post">
+            <div class="form-group">
+                <label for="naam">Naam:</label>
+                <input type="text" name="naam" placeholder="Naam:">
+            </div>
+            <div class="form-group">
+            <label for="Email">Email:</label>
+                <input type="email" name="email" placeholder="Email:">
+            </div>
+            <div class="form-group">
+            <label for="password">Password:</label>
+                <input type="password" name="password" placeholder="Password:">
+            </div>
+            <div class="form-group">
+            <label for="repear_password">Herhaal password</label>
+                <input type="password" name="repeat_password" placeholder="Herhaal passwoord:">
+            </div>
+            <div class="form-btn">
+                <input type="submit" gvalue="Register" name="submit">
+            </div>
+        </form>
+        <div>
+        <div><p>Al geregistreerd?<a href="login.html">Log hier in!</a></p></div>
+      </div>
+    </div>
 
     <footer>
         <div class="footerlinks">
